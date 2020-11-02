d789e579dba507946dfea2e6149262a3a766df64
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2020-11-02 17:40:54 -0600

    Funcionalidad del Drawer Menu

--- ChordGalore/app/src/main/java/com/example/chordgalore/MainActivity.kt
+++ ChordGalore/app/src/main/java/com/example/chordgalore/MainActivity.kt
@@ -2,14 +2,34 @@ package com.example.chordgalore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.custom_toolbar.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Le decimos que utilice el toolbar que creamos
         setSupportActionBar(toolbar)
+
+        //Esto es para declarar la creación del botón para abrir el menú de manera automática
+        toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    //Esta funcion es llamada cuando le picamos al botón "<" del telefono
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)){
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }
+        else
+            super.onBackPressed()
     }
 }
