f7a429c269d2efafa33f5b9e66db2e0603fbfa64
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2020-11-01 19:18:06 -0600

    Toolbar y Floating Action Button

--- ChordGalore/app/src/main/java/com/example/chordgalore/MainActivity.kt
+++ ChordGalore/app/src/main/java/com/example/chordgalore/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.chordgalore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.custom_toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
     }
 }
