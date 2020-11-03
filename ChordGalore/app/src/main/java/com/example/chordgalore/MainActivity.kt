1b30d682e82c966ac3720175d867b82c95074422
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2020-11-02 19:30:57 -0600

    Logica del cambio de pantalla basica

--- ChordGalore/app/src/main/java/com/example/chordgalore/MainActivity.kt
+++ ChordGalore/app/src/main/java/com/example/chordgalore/MainActivity.kt
@@ -27,14 +27,33 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.open_drawer, R.string.close_drawer)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+
+        //Abrimos la pantalla de inicio como default en caso de que no haya ningun estado guardado
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment()).commit()
+            nav_view.setCheckedItem(R.id.nav_home)
+        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
 
-            R.id.nav_profile -> Toast.makeText(applicationContext, "Perfil Cliqueado", Toast.LENGTH_SHORT).show()
+            R.id.nav_home -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,HomeFragment()).commit()
+
+            R.id.nav_profile -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,ProfileFragment()).commit()
+
+            R.id.nav_favorite -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,FavoriteFragment()).commit()
+
+            R.id.nav_download -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,DownloadFragment()).commit()
+
+            R.id.nav_config -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,ConfigurationFragment()).commit()
 
         }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+
+        //Toast.makeText(applicationContext, "Perfil Cliqueado", Toast.LENGTH_SHORT).show()
         return true
     }
 
