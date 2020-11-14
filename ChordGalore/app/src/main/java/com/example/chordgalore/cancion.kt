a29fe4188b832747f89a706e03440361936089fc
PainChip <PainChip@users.noreply.github.com>
2020-11-14 17:39:02 -0600

    Pantalla Cancion

---
+++ ChordGalore/app/src/main/java/com/example/chordgalore/cancion.kt
@@ -0,0 +1,11 @@
+package com.example.chordgalore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cancion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cancion)
+    }
+}
