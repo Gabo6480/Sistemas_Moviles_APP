b41b80e0aa740b1e850c74d889db2d5fd129d2d8
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2020-12-06 00:58:22 -0600

    Pantalla de cancion con imagenes y texto

--- ChordGalore/app/src/main/java/com/example/chordgalore/SongActivity.kt
+++ ChordGalore/app/src/main/java/com/example/chordgalore/SongActivity.kt
@@ -1,7 +1,7 @@
 package com.example.chordgalore
 
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.custom_toolbar.*
 
@@ -25,5 +25,20 @@ class SongActivity : AppCompatActivity() {
                 Toast.LENGTH_LONG
             ).show()
         }
+
+        val imageContainer = findViewById<LinearLayout>(R.id.image_container)
+
+        repeat(3) {
+            val newImage = ImageView(this)
+            newImage.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT)
+            newImage.setImageResource(R.drawable.default_image)
+            newImage.scaleType = ImageView.ScaleType.CENTER_INSIDE
+            newImage.adjustViewBounds = true
+            newImage.setPadding(0,0,16,0)
+            imageContainer.addView(newImage)
+        }
+
+        val textoConatiner = findViewById<TextView>(R.id.TextoCancion)
+        textoConatiner.text = "Esla hora de jugar"
     }
 }
