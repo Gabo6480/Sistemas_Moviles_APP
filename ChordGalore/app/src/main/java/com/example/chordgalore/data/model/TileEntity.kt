4a24c6d90a78f3ce1bf6a70fe971aa06ad8e159c
AlfonsoMata <alfonsomata424@gmail.com>
2020-12-07 18:38:49 -0600

    Localgetpublicaciones

--- ChordGalore/app/src/main/java/com/example/chordgalore/data/model/TileEntity.kt
+++ ChordGalore/app/src/main/java/com/example/chordgalore/data/model/TileEntity.kt
@@ -2,4 +2,4 @@ package com.example.chordgalore.data.model
 
 import android.graphics.Bitmap
 
-data class TileEntity(val id : Int, val image : Bitmap, val title : String, val subtitle : String, val genre : String)
+data class TileEntity(val id: Int, val image: Bitmap, val title: String, val subtitle: String, val genre: String)
